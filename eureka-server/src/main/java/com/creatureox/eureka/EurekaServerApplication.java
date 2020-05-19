a8e41073df71a7c047472e1fbb64ef4e25b71bbc
CreatureOX <creatureox0726@gmail.com>
2020-05-19 23:38:58 +0800

    eureka server

---
+++ eureka-server/src/main/java/com/creatureox/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.creatureox.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author CreatureOX
+ * date: 2020/5/19
+ * description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
